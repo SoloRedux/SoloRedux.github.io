d9512ba67973962844aa85667190031c14ced314
Ais Laksana <aislakana@gmail.com>
2024-03-08 22:37:08 +0700

    perbaikan

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
- const mntoggle = document.querySelector('.menu-toggle input');
- const nav = document.querySelector('nav ul');
+const mntoggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
 mntoggle.addEventListener('click',function(){
-    nav.classList.toggle('menushow');
+   nav.classList.toggle('menushow');
 })
+
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-links");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab");
+}
