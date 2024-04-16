9e9d7b84d75b488913bfd76cbcbce94969a88860
Ais Laksana <aislakana@gmail.com>
2024-04-16 12:55:50 +0700

    Scrape data

--- js/main.js
+++ js/main.js
@@ -8,6 +8,28 @@ mntoggle.addEventListener('click',function(){
 })
 
 
+$(function () {
+    const filePath = 'scrap_data/scrap.json';
+    $.get(filePath, function (data) {
+        var str = "<table border=1>";
+        str += "<tr><td>No</td><td>Judul</td><td>Kategori</td><td>Waktu Publish</td><td>Waktu Scraping</td></tr>";
+
+        $.each(data, function (n, item) {
+            str += "<tr>";
+            str += "<td>" + (n + 1) + "</td>";
+            str += "<td>" + item.Judul + "</td>";
+            str += "<td>" + item.Kategori + "</td>";
+            str += "<td>" + item['Waktu-Publish'] + "</td>";
+            str += "<td>" + item['Waktu-Scrapping'] + "</td>";
+            str += "</tr>";
+        });
+        str += "</tabel>";
+        $('#data').html(str);
+    });
+});
+
+
+
 function opentab(tabname){
     for(tablink of tablinks){
         tablink.classList.remove("active-links");
@@ -18,3 +40,4 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-links");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
